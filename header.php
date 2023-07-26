16f6dc17ddc1c1713238e046ecab90caa552639b
Eric Busch <ericbusch@gmail.com>
2023-07-26 17:08:33 -0400

    prod build

--- header.php
+++ header.php
@@ -5,11 +5,13 @@
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Caveat&family=Inter:wght@200;400;700&family=Jost:wght@200;300;400;500;600;700;900&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class( 'bg-white text-gray-900 antialiased' ); ?>>
+<body <?php body_class( 'bg-gradient-to-b from-gray-50 to-gray-100 bg-fixed font-sans' ); ?>>
 
 <?php do_action( 'tailpress_site_before' ); ?>
 
@@ -19,14 +21,51 @@
 
 	<header>
 
-		<div class="mx-auto container">
+		<!-- Navigation -->
+		<section class="border-b border-gray-200 bg-white default-margin-b">
+			<div class="flex flex-col items-start py-6 sm:flex-row sm:items-center sm:justify-between sm:py-12 default-width default-padding-x">
+
+				<div class="w-full sm:w-3/12">
+					<h1 class="text-center sm:text-left">
+						<a href="/" class="font-heading text-xl font-normal uppercase tracking-lg text-gray-700  w-full">Eric<span class="font-semibold">Busch</span> </a>
+					</h1>
+				</div>
+
+				<div class="bg-teal-40 w-full sm:w-6/12">
+					<ul class="flex flex-row items-center justify-center space-x-5 font-heading text-sm uppercase tracking-lg sm:mt-0 sm:space-x-3 sm:tracking-widest md:space-x-8 md:tracking-lg">
+						<li><a class="block py-2 leading-none text-gray-500" href="#">Collections</a></li>
+						<li><a class="block py-2 leading-none text-gray-500" href="#">Blog</a></li>
+						<li><a class="block py-2 leading-none text-gray-500" href="#">About</a></li>
+					</ul>
+				</div>
+
+				<div class="bg-amber-40 w-full sm:w-3/12">
+					<div class="flex flex-row items-center justify-center space-x-3 sm:justify-end">
+						<a href="#" class="py-2 leading-none sm:py-1.5" target="_blank" rel="noopener">
+							<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+								<path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+							</svg>
+						</a>
+
+						<a href="#" class="py-2 leading-none sm:py-1.5" target="_blank" rel="noopener">
+							<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+								<path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" />
+							</svg>
+						</a>
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+		<div class="">
 			<div class="lg:flex lg:justify-between lg:items-center border-b py-6">
 				<div class="flex justify-between items-center">
 					<div>
 						<?php if ( has_custom_logo() ) { ?>
                             <?php the_custom_logo(); ?>
 						<?php } else { ?>
-							<a href="<?php echo get_bloginfo( 'url' ); ?>" class="font-extrabold text-lg uppercase">
+							<a href="<?php echo get_bloginfo( 'url' ); ?>" class="font-heading text-lg uppercase">
 								<?php echo get_bloginfo( 'name' ); ?>
 							</a>
 
@@ -69,25 +108,6 @@
 	</header>
 
 	<div id="content" class="site-content flex-grow">
-
-		<?php if ( is_front_page() ) { ?>
-			<!-- Start introduction -->
-			<div class="container mx-auto">
-				<div class="px-12 py-16 my-12 rounded-xl bg-gradient-to-r from-blue-50 from-10% via-sky-100 via-30% to-blue-200 to-90%">
-                    <div class="mx-auto max-w-screen-md">
-                        <h1 class="text-3xl lg:text-6xl tracking-tight font-extrabold text-gray-800 mb-6">Start building your next <a href="https://tailwindcss.com" class="text-secondary">Tailwind CSS</a> flavoured WordPress theme
-                            with <a href="https://tailpress.io" class="text-primary">TailPress</a>.</h1>
-                        <p class="text-gray-600 text-xl font-medium mb-10">TailPress is your go-to starting
-                            point for developing WordPress themes with Tailwind CSS and comes with basic block-editor support out
-                            of the box.</p>
-                        <a href="https://github.com/jeffreyvr/tailpress"
-                            class="w-full sm:w-auto flex-none bg-gray-900 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">View
-                            on GitHub</a>
-                    </div>
-                </div>
-			</div>
-			<!-- End introduction -->
-		<?php } ?>
 
 		<?php do_action( 'tailpress_content_start' ); ?>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const theme = require('./theme.json');
 const tailpress = require("@jeffreyvr/tailwindcss-tailpress");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -11,25 +12,22 @@ module.exports = {
         './safelist.txt'
     ],
     theme: {
-        container: {
-            padding: {
-                DEFAULT: '1rem',
-                sm: '2rem',
-                lg: '0rem'
-            },
-        },
         extend: {
-            colors: tailpress.colorMapper(tailpress.theme('settings.color.palette', theme)),
-            fontSize: tailpress.fontSizeMapper(tailpress.theme('settings.typography.fontSizes', theme))
+            fontFamily: {
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+                heading: ['Jost', ...defaultTheme.fontFamily.sans],
+                handwriting: ['Caveat', ...defaultTheme.fontFamily.serif],
+            },
+            letterSpacing: {
+                'md': '.15em',
+                'lg': '.20em',
+                'xl': '.30em',
+                '2xl': '.35em',
+                '3xl': '.40em',
+                '4xl': '.45em',
+                '5xl': '.5em',
+            }
         },
-        screens: {
-            'xs': '480px',
-            'sm': '600px',
-            'md': '782px',
-            'lg': tailpress.theme('settings.layout.contentSize', theme),
-            'xl': tailpress.theme('settings.layout.wideSize', theme),
-            '2xl': '1440px'
-        }
     },
     plugins: [
         tailpress.tailwind

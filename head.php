4ef91d863e31309f754b9e4434643cd9e1d1344b
Eric Busch <ericbusch@gmail.com>
2023-07-26 21:50:31 -0400

    added homepage

---
+++ head.php
@@ -0,0 +1,13 @@
+<head>
+	<?php include_once get_template_directory() . '/ascii-message.html'; ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Caveat&family=Inter:wght@200;400;700&family=Jost:wght@200;300;400;500;600;700;900&display=swap"
+		rel="stylesheet">
+	<?php wp_head(); ?>
+</head>
--- header.php
+++ header.php
@@ -1,16 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-	<?php include_once get_template_directory() . '/ascii-message.html'; ?>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Caveat&family=Inter:wght@200;400;700&family=Jost:wght@200;300;400;500;600;700;900&display=swap" rel="stylesheet">
-	<?php wp_head(); ?>
-</head>
+<?php include_once get_template_directory() . '/head.php'; ?>
 
 <body <?php body_class( 'bg-gradient-to-b from-gray-50 to-gray-100 bg-fixed font-sans' ); ?>>
 
@@ -24,92 +14,64 @@
 
 		<!-- Navigation -->
 		<section class="border-b border-gray-200 bg-white default-margin-b">
-			<div class="flex flex-col items-start py-6 sm:flex-row sm:items-center sm:justify-between sm:py-12 default-width default-padding-x">
+			<div
+				class="flex flex-col items-start py-6 sm:flex-row sm:items-center sm:justify-between sm:py-12 default-width default-padding-x">
 
+				<!-- Site Title -->
 				<div class="w-full sm:w-3/12">
 					<h1 class="text-center sm:text-left">
-						<a href="<?php echo get_bloginfo( 'url' ); ?>" title="Return to homepage" class="font-heading text-xl font-normal uppercase tracking-lg text-gray-700  w-full">Eric<span class="font-semibold">Busch</span> </a>
+						<a href="<?php echo get_bloginfo( 'url' ); ?>"
+						   title="Return to homepage"
+						   class="font-heading text-xl font-normal uppercase tracking-lg text-gray-700">
+							<span>Eric</span><span class="font-semibold">Busch</span>
+						</a>
 					</h1>
 				</div>
+				<!-- /Site Title -->
 
-				<div class="bg-teal-40 w-full sm:w-6/12">
-					<ul class="flex flex-row items-center justify-center space-x-5 font-heading text-sm uppercase tracking-lg sm:mt-0 sm:space-x-3 sm:tracking-widest md:space-x-8 md:tracking-lg">
-						<li><a class="block py-2 leading-none text-gray-500" href="#">Collections</a></li>
-						<li><a class="block py-2 leading-none text-gray-500" href="#">Blog</a></li>
-						<li><a class="block py-2 leading-none text-gray-500" href="#">About</a></li>
-					</ul>
+				<!-- Primary Menu -->
+				<div class="w-full sm:w-6/12">
+					<?php wp_nav_menu( [
+						'container_id'   => 'primary-menu',
+						'menu_class'     => 'flex flex-row items-center justify-center space-x-5 font-heading text-sm uppercase tracking-lg sm:mt-0 sm:space-x-3 sm:tracking-widest md:space-x-8 md:tracking-lg',
+						'theme_location' => 'primary',
+					] ); ?>
 				</div>
+				<!-- /Primary Menu -->
 
-				<div class="bg-amber-40 w-full sm:w-3/12">
+				<!-- Contact Icons -->
+				<div class="w-full sm:w-3/12">
 					<div class="flex flex-row items-center justify-center space-x-3 sm:justify-end">
-						<a href="#" class="py-2 leading-none sm:py-1.5" target="_blank" rel="noopener">
-							<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-gray-500" viewBox="0 0 448 512">
-								<path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
-							</svg>
-						</a>
-
-						<a href="#" class="py-2 leading-none sm:py-1.5" target="_blank" rel="noopener">
-							<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-gray-500" viewBox="0 0 512 512">
-								<path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" />
-							</svg>
-						</a>
+						<?php if ( $instagram_url = get_field( 'instagram_url', 'option' ) ) : ?>
+							<a href="<?php echo esc_url( $instagram_url ); ?>" class="py-2 leading-none sm:py-1.5"
+							   target="_blank" rel="noopener">
+								<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-gray-500"
+								     viewBox="0 0 448 512">
+									<path
+										d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+								</svg>
+							</a>
+						<?php endif; ?>
+						<?php if ( $contact_email = get_field( 'contact_email', 'option' ) ) : ?>
+							<a href="mailto:<?php esc_html_e( $contact_email ); ?>"
+							   class="py-2 leading-none sm:py-1.5" target="_blank" rel="noopener">
+								<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-gray-500"
+								     viewBox="0 0 512 512">
+									<path
+										d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/>
+								</svg>
+							</a>
+						<?php endif; ?>
 					</div>
 				</div>
+				<!-- /Contact Icons -->
 
 			</div>
 		</section>
+		<!-- /Navigation -->
 
-		<div class="">
-			<div class="lg:flex lg:justify-between lg:items-center border-b py-6">
-				<div class="flex justify-between items-center">
-					<div>
-						<?php if ( has_custom_logo() ) { ?>
-                            <?php the_custom_logo(); ?>
-						<?php } else { ?>
-							<a href="<?php echo get_bloginfo( 'url' ); ?>" class="font-heading text-lg uppercase">
-								<?php echo get_bloginfo( 'name' ); ?>
-							</a>
-
-							<p class="text-sm font-light text-gray-600">
-								<?php echo get_bloginfo( 'description' ); ?>
-							</p>
-
-						<?php } ?>
-					</div>
-
-					<div class="lg:hidden">
-						<a href="#" aria-label="Toggle navigation" id="primary-menu-toggle">
-							<svg viewBox="0 0 20 20" class="inline-block w-6 h-6" version="1.1"
-								 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-								<g stroke="none" stroke-width="1" fill="currentColor" fill-rule="evenodd">
-									<g id="icon-shape">
-										<path d="M0,3 L20,3 L20,5 L0,5 L0,3 Z M0,9 L20,9 L20,11 L0,11 L0,9 Z M0,15 L20,15 L20,17 L0,17 L0,15 Z"
-											  id="Combined-Shape"></path>
-									</g>
-								</g>
-							</svg>
-						</a>
-					</div>
-				</div>
-
-				<?php
-				wp_nav_menu(
-					array(
-						'container_id'    => 'primary-menu',
-						'container_class' => 'hidden bg-gray-100 mt-4 p-4 lg:mt-0 lg:p-0 lg:bg-transparent lg:block',
-						'menu_class'      => 'lg:flex lg:-mx-4',
-						'theme_location'  => 'primary',
-						'li_class'        => 'lg:mx-4',
-						'fallback_cb'     => false,
-					)
-				);
-				?>
-			</div>
-		</div>
 	</header>
 
 	<div id="content" class="site-content flex-grow">
-
 		<?php do_action( 'tailpress_content_start' ); ?>
-
 		<main>

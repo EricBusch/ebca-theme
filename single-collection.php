fbe67cf198d6e3156cc9889e8695dbe4916320c8
Eric Busch <ericbusch@gmail.com>
2023-07-30 07:48:11 -0400

    working gallery layout

--- single-collection.php
+++ single-collection.php
@@ -10,52 +10,43 @@
 			] ); ?>
 
 			<!-- NEW LAYOUT -->
-			<div class="flex max-w-[88rem] mx-auto flex-col justify-evenly sm:gap-8 sm:px-8">
+			<div class="flex max-w-[82rem] mx-auto flex-col justify-evenly sm:gap-8 sm:px-8 default-margin-b">
 
 				<?php
 				$count = 0;
 				$html  = '';
+				$hr    = '<hr class="sm:hidden border-gray-200 w-full" />';
 				?>
-				<?php foreach ( busch_get_collection_images( get_the_ID(), 'large', [ 'class' => 'max-h-[80vh] shadow w-auto' ] ) as $image ) : ?>
+				<?php foreach ( busch_get_collection_images( get_the_ID(), 'large', [ 'class' => 'max-h-[76vh] shadow w-auto' ] ) as $image ) : ?>
 					<?php
-					$html  .= '<a href="#" class="bg-white p-8 sm:shadow">' . $image->_html . '</a>';
 					$count = $image->_orientation !== 'portrait' ? $count += 2 : $count += 1;
-					if ( $count < 2 ) {
-						continue;
+					$html  .= '<a href="#" class="bg-white p-8 sm:shadow">' . $image->_html . '</a>';
+
+					if ( $image->_orientation === 'portrait' ) {
+						if ( $image->_next_orientation === 'portrait' ) {
+							if ( $count < 2 ) {
+								$html .= $hr;
+								continue;
+							}
+						}
 					}
 					?>
 					<div
 						class="flex flex-col items-center justify-center bg-white sm:gap-8 sm:bg-transparent md:flex-row">
 						<?php echo $html; ?>
 					</div>
+
+					<?php echo $hr; ?>
+
 					<?php
 					$count = 0;
 					$html  = '';
 					?>
 				<?php endforeach; ?>
-
-				<hr class="sm:hidden border-gray-200 w-full"/>
 			</div>
 			<!-- /NEW LAYOUT -->
 
 
-			<!-- Display Images -->
-			<section class="mx-auto md:max-w-3xl lg:max-w-8xl px-0 sm:px-10 md:px-12 default-margin-b">
-				<div class="flex flex-wrap items-center gap-x-6 gap-y-px sm:gap-y-[8vh]">
-
-					<?php foreach ( busch_get_collection_images( get_the_ID(), 'large', [ 'class' => 'max-h-[70vh] shadow w-full h-auto' ] ) as $image ) : ?>
-						<div
-							class="flex items-center justify-center lg:justify-evenly w-full mx-auto bg-white sm:bg-transparent sm:w-auto">
-							<div
-								class="flex justify-center bg-white px-8 py-8 sm:inline-flex w-auto sm:justify-normal sm:shadow-sm">
-								<?php echo $image->_html; ?>
-							</div>
-						</div>
-					<?php endforeach; ?>
-
-				</div>
-			</section>
-			<!-- /Display Images -->
 		<?php endwhile; ?>
 	<?php endif; ?>
 </div>

3ee9af0d47e40f28a84b9d91a0868e0056a3e264
Eric Busch <ericbusch@gmail.com>
2023-07-30 08:18:08 -0400

    Update single-collection.php

--- single-collection.php
+++ single-collection.php
@@ -9,43 +9,44 @@
 				'text'  => strip_tags( get_the_content(), [ 'a', 'strong', 'em', ] ),
 			] ); ?>
 
-			<!-- NEW LAYOUT -->
+			<!-- Display Images from Collection -->
 			<div class="flex max-w-[82rem] mx-auto flex-col justify-evenly sm:gap-8 sm:px-8 default-margin-b">
-
 				<?php
 				$count = 0;
 				$html  = '';
 				$hr    = '<hr class="sm:hidden border-gray-200 w-full" />';
-				?>
-				<?php foreach ( busch_get_collection_images( get_the_ID(), 'large', [ 'class' => 'max-h-[76vh] shadow w-auto' ] ) as $image ) : ?>
-					<?php
+				$link  = '<a href="#" class="bg-white p-8 sm:shadow" title="View larger">%s</a>';
+				$row   = '<!-- Row Start --><div class="flex flex-col items-center justify-center bg-white sm:gap-8 sm:bg-transparent md:flex-row">%s</div><!-- /Row Start -->';
+
+				foreach ( busch_get_formatted_images_for_collection( get_the_ID(), 'large', [ 'class' => 'max-h-[76vh] shadow w-auto' ] ) as $image ) {
+
+					// Count "1" for a portrait image, "2" for a non-portrait image.
 					$count = $image->_orientation !== 'portrait' ? $count += 2 : $count += 1;
-					$html  .= '<a href="#" class="bg-white p-8 sm:shadow">' . $image->_html . '</a>';
 
-					if ( $image->_orientation === 'portrait' ) {
-						if ( $image->_next_orientation === 'portrait' ) {
-							if ( $count < 2 ) {
-								$html .= $hr;
-								continue;
-							}
-						}
+					// Build <a> link for image.
+					$html .= sprintf( $link, $image->_html );
+
+					// If the current image is a portrait and the next image is a portrait and we have only added 1 image to this row...
+					if ( $image->_orientation === 'portrait' && $image->_next_orientation === 'portrait' && $count < 2 ) {
+
+						// Insert <hr> tag and continue to get the next image in loop before closing "row" div.
+						$html .= $hr;
+						continue;
 					}
-					?>
-					<div
-						class="flex flex-col items-center justify-center bg-white sm:gap-8 sm:bg-transparent md:flex-row">
-						<?php echo $html; ?>
-					</div>
 
-					<?php echo $hr; ?>
+					// Build "row" div.
+					printf( $row, $html );
 
-					<?php
+					// Display <hr> between images.
+					echo $hr;
+
+					// Reset $count and $html.
 					$count = 0;
 					$html  = '';
-					?>
-				<?php endforeach; ?>
+				}
+				?>
 			</div>
-			<!-- /NEW LAYOUT -->
-
+			<!-- /Display Images from Collection -->
 
 		<?php endwhile; ?>
 	<?php endif; ?>

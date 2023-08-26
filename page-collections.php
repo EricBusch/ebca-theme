ce92d9e13ee2b241d08bc313ce936b70b7c5de2b
Eric Busch <ericbusch@gmail.com>
2023-08-26 12:41:41 -0400

    tweaked collections page layout

--- page-collections.php
+++ page-collections.php
@@ -16,39 +16,39 @@
 					<?php $new_images = busch_get_new_image_ids_for_collection( $collection->ID, get_field( 'image_new_in_days', 'option' ) ); ?>
 					<a href="<?php echo esc_url( get_permalink( $collection->ID ) ); ?>"
 					   title="View images in this collection"
-					   class="grid aspect-[16/9] grid-cols-1 grid-rows-1 flex-col justify-center rounded bg-slate-800 hover:bg-black bg-opacity-50 bg-cover bg-center bg-no-repeat p-8 text-white saturate-[.15] hover:saturate-100 bg-blend-multiply shadow-md transition duration-300 hover:bg-opacity-20 hover:shadow-sm lg:aspect-[4/5]"
-					   style="background-image: url(<?php echo get_the_post_thumbnail_url( $collection->ID, 'large' ); ?>);">
+					   class="grid grid-cols-1 grid-rows-6 gap-2 aspect-square bg-white pt-8 px-8 pb-4 shadow">
 						<div
-							class="flex h-full w-full flex-row items-start justify-start space-x-1 font-normal text-white">
-							<div class="flex h-full w-full flex-col justify-between space-y-1">
-								<div class="flex flex-col space-y-1">
-									<div
-										class="m-0 p-0 text-xl font-semibold uppercase leading-none tracking-md sm:text-lg md:text-xl drop-shadow">
-										<?php esc_html_e( $collection->post_title ); ?>
-										<?php if ( $collection->post_status !== 'publish' ) : ?>
-											<span
-												class="inline-flex items-center rounded-full bg-red-100 px-1.5 py-0.5 text-xs font-medium text-red-700 tracking-tight font-sans lowercase">
+							class="row-span-4 flex h-full w-full flex-row items-start justify-start space-x-1 font-normal text-white bg-cover bg-center bg-no-repeat bg-blend-multiply shadow saturate-[.15]"
+							style="background-image: url(<?php echo get_the_post_thumbnail_url( $collection->ID, 'large' ); ?>);">
+						</div>
+						<div class="row-span-2 flex flex-row items-center h-full">
+							<div class="flex flex-col space-y-1">
+								<div
+									class="m-0 p-0 font-semibold uppercase leading-none tracking-wider">
+									<?php esc_html_e( $collection->post_title ); ?>
+									<?php if ( $collection->post_status !== 'publish' ) : ?>
+										<span
+											class="inline-flex items-center rounded-full bg-red-100 px-1.5 py-0.5 text-xs font-medium text-red-700 tracking-tight font-sans lowercase">
 												<?php esc_html_e( $collection->post_status ); ?>
 											</span>
-										<?php endif; ?>
-									</div>
-									<div
-										class="m-0 p-0 text-sm uppercase leading-none tracking-wider text-gray-200 drop-shadow">
-										<?php $image_count = busch_get_image_count_for_collection( $collection->ID ); ?>
-										<?php esc_html_e( $image_count ); ?>
-										<?php echo $image_count === 1 ? 'image' : 'images'; ?>
-									</div>
-									<?php if ( is_array( $new_images ) && count( $new_images ) > 0 ) : ?>
-										<div>
+									<?php endif; ?>
+								</div>
+								<div
+									class="m-0 p-0 text-sm uppercase leading-none tracking-wider text-gray-500">
+									<?php $image_count = busch_get_image_count_for_collection( $collection->ID ); ?>
+									<?php esc_html_e( $image_count ); ?>
+									<?php echo $image_count === 1 ? 'image' : 'images'; ?>
+								</div>
+								<?php if ( is_array( $new_images ) && count( $new_images ) > 0 ) : ?>
+									<div>
 											<span
 												class="mt-1 inline-flex items-center rounded-md border border-gray-400 px-1.5 py-0.5 text-xs font-medium text-gray-200 shadow">
 												<?php esc_html_e( count( $new_images ) ); ?> new
 											</span>
-										</div>
-									<?php endif; ?>
-								</div>
+									</div>
+								<?php endif; ?>
 								<div
-									class="m-0 mt-auto p-0 text-right text-xs uppercase leading-none tracking-wider text-gray-200 drop-shadow">
+									class="text-xs uppercase leading-none tracking-wider text-gray-500">
 									Updated <?php esc_html_e( date( 'F Y', strtotime( $collection->post_modified ) ) ); ?>
 								</div>
 							</div>

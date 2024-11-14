c07b0bd26718414728a200c6f3c5588485f82932
Eric Busch <ericbusch@gmail.com>
2024-11-13 21:43:23 -0500

    fixing incorrectly sized images

--- template-parts/collection-flexible-content-gallery.php
+++ template-parts/collection-flexible-content-gallery.php
@@ -1,16 +1,21 @@
 <?php
 /** @var int[] $attachment_ids */
 $attachment_ids = $args['attachment_ids'] ?? [];
-$count = 0;
-$html  = '';
-$hr    = '<hr class="sm:hidden border-gray-200 w-full" />';
-$link  = '<a href="%s" class="bg-white p-8 sm:shadow" title="View larger" data-fslightbox>%s</a>';
-$row   = '<!-- Row Start --><div class="flex flex-col items-center justify-center bg-white sm:gap-8 sm:bg-transparent md:flex-row">%s</div><!-- /Row Start -->';
+$count          = 0;
+$html           = '';
+$hr             = '<hr class="sm:hidden border-gray-200 w-full" />';
+$link           = '<a href="%s" class="bg-white p-8 sm:shadow" title="View larger" data-fslightbox>%s</a>';
+$row            = '<!-- Row Start --><div class="flex flex-col items-center justify-center bg-white sm:gap-8 sm:bg-transparent md:flex-row">%s</div><!-- /Row Start -->';
 ?>
 <!-- Display Images from Collection -->
 <div
 	class="flex max-w-[82rem] mx-auto flex-col justify-evenly sm:gap-x-8 sm:gap-y-8 lg:gap-y-12 sm:px-8 default-margin-b">
-	<?php foreach ( busch_get_gallery_images( $attachment_ids, '2048x2048', [ 'class' => 'max-h-[76vh] shadow w-auto' ] ) as $image ) {
+	<?php foreach (
+		busch_get_gallery_images( $attachment_ids, '2048x2048', [
+			'class' => 'max-h-[76vh] shadow w-auto',
+			'loading' => false, // Added this on 2024-11-13 to fix images getting shrunken if they had a loading="lazy" attribute on them.
+		] ) as $image
+	) {
 
 		// Count "1" for a portrait image, "2" for a non-portrait image.
 		$count = $image->_orientation !== 'portrait' ? $count += 2 : $count += 1;
